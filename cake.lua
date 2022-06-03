587c3ec2818df8663ec44182061ac3e4bd1d1ebc
Manuel8090 <105463338+Manuel8090@users.noreply.github.com>
2022-06-02 19:51:18 -0600

    Update cake.lua

--- cake.lua
+++ cake.lua
@@ -3,6 +3,8 @@ if game.PlaceId == 536102540 or 3565304751 then
         while wait() do
             local Rootpart = game.Players.LocalPlayer.Character.HumanoidRootPart
             Rootpart.CFrame = CFrame.new(game.Workspace.Live.juan100m1.HumanoidRootPart.Position)
+        end
+        end)
     coroutine.resume(tp)
     
     local tp1 = coroutine.create(function()
