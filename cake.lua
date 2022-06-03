3a7d8da05491ccfde122e4ad21d726b59df50d57
Manuel8090 <105463338+Manuel8090@users.noreply.github.com>
2022-06-02 19:16:41 -0600

    Update cake.lua

--- cake.lua
+++ cake.lua
@@ -1,5 +1,16 @@
 if game.PlaceId == 536102540 or 3565304751 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Manuel8090/auto-tp/main/auto%20tp", true))()
+    local tp = coroutine.create(function()
+        while wait() do
+            local Rootpart = game.Players.LocalPlayer.Character.HumanoidRootPart
+            Rootpart.CFrame = CFrame.new(game.Workspace.Live.terrellmoney13.HumanoidRootPart.Position)
+        end
+    end)
+    coroutine.resume(tp)
+    
+    local tp1 = coroutine.create(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Manuel8090/auto-tp/main/auto%20tp", true))()
+    end)
+    coroutine.resume(tp1)   
 elseif game.PlaceId == 2050207304 then
         local Broly = "Broly BR"
         repeat wait() until game:GetService("Workspace").Live[Broly].Humanoid.Health < 0.8
