c149dd9258c25f1554a99da6238238fcfe6373e3
Manuel8090 <105463338+Manuel8090@users.noreply.github.com>
2022-06-02 19:18:54 -0600

    Update cake.lua

--- cake.lua
+++ cake.lua
@@ -10,8 +10,9 @@ if game.PlaceId == 536102540 or 3565304751 then
     local tp1 = coroutine.create(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Manuel8090/auto-tp/main/auto%20tp", true))()
     end)
-    coroutine.resume(tp1)   
-elseif game.PlaceId == 2050207304 then
+    coroutine.resume(tp1)
+end
+if game.PlaceId == 2050207304 then
         local Broly = "Broly BR"
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Manuel8090/host-for-tp-cframe/main/ddaddy%20bread", true))()
 end
