b1af755e716bd75d79f3c23113903295ea3a8384
Manuel8090 <105463338+Manuel8090@users.noreply.github.com>
2022-06-02 20:18:12 -0600

    Update cake.lua

--- cake.lua
+++ cake.lua
@@ -1,12 +1,4 @@
 if game.PlaceId == 536102540 or 3565304751 then
-    local tp = coroutine.create(function()
-        while wait() do
-            local Rootpart = game.Players.LocalPlayer.Character.HumanoidRootPart
-            Rootpart.CFrame = CFrame.new(game.Workspace.Live.juan100m1.HumanoidRootPart.Position)
-        end
-        end)
-    coroutine.resume(tp)
-    
     local tp1 = coroutine.create(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Manuel8090/auto-tp/main/auto%20tp", true))()
     end)
