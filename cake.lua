61c5d450afb4dd428ea09b5b58934b3da9ed9e9b
Manuel8090 <105463338+Manuel8090@users.noreply.github.com>
2022-06-02 19:00:46 -0600

    Update cake.lua

--- cake.lua
+++ cake.lua
@@ -1,37 +1,6 @@
 if game.PlaceId == 536102540 or 3565304751 then
-        while wait(.5) do 
-        wait(.8)
-        keypress(0x31)--1
-        wait()
-        keyrelease(0x31)--1
-        wait(.5)
-        keypress(0x31)--1
-        wait(.5)
-        keypress(0xBF)--/
-        wait()
-        keypress(0x4A)--j
-        wait()
-        keypress(0x55)--u
-        wait()
-        keypress(0x41)--a
-        wait()
-        keypress(0x4E)--n
-        wait()
-        keypress(0x31)--1
-        wait()
-        keypress(0x30)--0
-        wait()
-        keypress(0x30)--0
-        wait()
-        keypress(0x4D)--m
-        wait()
-        keypress(0x31)--1
-        wait()
-        keypress(0x0D)
-	wait()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Manuel8090/host-for-tp-cframe/main/ddaddy%20bread", true))()
-    end
-    elseif game.PlaceId == 2050207304 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Manuel8090/auto-tp/main/auto%20tp", true))()
+elseif game.PlaceId == 2050207304 then
         local Broly = "Broly BR"
         repeat wait() until game:GetService("Workspace").Live[Broly].Humanoid.Health < 0.8
             wait(.1)
